ff9fa3489aa55def914ce621785c826fc4263cc1
Premila Anand <panand71@gmail.com>
2016-01-07 15:06:57 -0800

    Adds redirect to projects page if project not found

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
   def index
     @projects = Project.all
   end
@@ -20,15 +22,12 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    @project = Project.find(params[:id])
   end
 
   def edit
-    @project = Project.find(params[:id])
   end
 
   def update
-    @project = Project.find(params[:id])
     @project.update(project_params)
 
     if @project.save
@@ -41,7 +40,6 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project = Project.find(params[:id])
     @project.destroy
 
     flash[:notice] = "Project has been deleted."
@@ -50,9 +48,15 @@ class ProjectsController < ApplicationController
 
 
   private
+  def set_project
+    @project = Project.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The project you were looking for could not be found."
+    redirect_to projects_path
+  end
 
-    def project_params
-      params.require(:project).permit(:name, :description)
-    end
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
 
 end

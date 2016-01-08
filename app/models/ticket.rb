f54e8f72da0d3c28313c2b61cb20821d7515e884
Premila Anand <panand71@gmail.com>
2016-01-08 15:36:19 -0800

    links tickets to users

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,4 +2,5 @@ class Ticket < ActiveRecord::Base
   belongs_to :project
   validates :name, presence: true
   validates :description, presence: true, length: { minimum: 10 }
+  belongs_to :author, class_name: "User"
 end
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,6 +1,17 @@
+# FactoryGirl.define do
+#   factory :user do
+#     sequence(:email) { |n| "test#{n}@example.com" }
+#     password "passwords"
+#   end
+# end
+
 FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "test#{n}@example.com" }
     password "password"
+
+    trait :admin do
+      admin true
+    end
   end
 end

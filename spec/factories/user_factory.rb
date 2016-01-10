df5d29f0bc9dff5ac9264d129a8b65e6c3753b20
Premila Anand <panand71@gmail.com>
2016-01-09 19:05:49 -0800

    adds application controller within admin namespace

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "test#{n}@example.com" }
-    password "password"
+    password "passwords"
     trait :admin do
       admin true
     end

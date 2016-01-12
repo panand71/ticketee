6d617da2b3e16d5954be38227c179e9a027a6aab
Premila Anand <panand71@gmail.com>
2016-01-11 18:50:47 -0800

    adds edit and update users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,5 @@
 class Admin::UsersController < Admin::ApplicationController
+  before_action   :set_user, only: [:show, :edit, :update, :destroy]
   def index
     @users = User.order(:email)
   end
@@ -19,10 +20,37 @@ class Admin::UsersController < Admin::ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if params[:user][:password].blank?
+      params[:user].delete(:password)
+    end
+    
+    if @user.update(user_params)
+      flash[:notice] = "User has been updated."
+      redirect_to admin_users_path
+    else
+      flash.now[:alert] = "User could not be updated."
+      render 'edit'
+    end
+  end
+
+
+
   private
-    def user_params
-      params.require(:user).permit(:email, :password, :admin)
-    end  
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :admin)
+  end  
 
 
 end

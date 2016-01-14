fefe0fce00005b8038e8dcbc42b0efa0d7e5ff68
Premila Anand <panand71@gmail.com>
2016-01-13 18:12:09 -0800

    prevents archived users from logging in

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,13 @@ class User < ActiveRecord::Base
 
   def archive
     self.update(archived_at: Time.now)
-  end         
+  end
+
+  def active_for_authentication?
+    super && archived_at.nil?
+  end
+
+  def inactive_message
+    archived_at.nil? ? super : :archived
+  end           
 end
